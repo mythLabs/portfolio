21b68551ec9e4e5423016d318007d2747c2b7665
mythLabs <amitbisht744@gmail.com>
2019-07-28 19:59:54 +0530

    created blog page dynamically and added links through individual slugs on blogs pagev2

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {graphql, useStaticQuery } from "gatsby"
+import {Link, graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 
@@ -16,6 +16,9 @@ const BlogPage = () => {
               }
               html
               excerpt
+              fields{
+                slug
+              }
             }
           }
         }
@@ -30,8 +33,10 @@ const BlogPage = () => {
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return(
                         <li>
-                            <h2>{edge.node.frontmatter.title}</h2>
-                            <p>{edge.node.frontmatter.date}</p>
+                            <Link to={`/blog/${edge.node.fields.slug}`}>
+                                <h2>{edge.node.frontmatter.title}</h2>
+                             <p>{edge.node.frontmatter.date}</p>
+                            </Link>
                         </li>
                     )
                 })}
---
+++ src/templates/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const Blog = () => {
+    return (
+       <Layout>
+           This is a single blog's template
+       </Layout>
+    )
+}
+
+export default Blog;
