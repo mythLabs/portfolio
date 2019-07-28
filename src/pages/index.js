6c1e91c12f11b48686ee309545809c4388278bff
mythLabs <amitbisht744@gmail.com>
2019-07-28 13:00:33 +0530

    Adding a common layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from "react"
+import { Link } from "gatsby"
 
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Layout from "../components/layout"
+
+
 
 const IndexPage = () => {
-    return(
-        <div>
-            <Header/>
-           <h2> mythLabs - Amit bisht - full stack developer</h2>
-           <p>Need help? <Link to="/contact">Contact me</Link></p>
-           <Footer/>
-        </div>
-    )
+  return (
+    <div>
+      <Layout>
+        <h2> mythLabs - Amit bisht - full stack developer</h2>
+        <p>
+          Need help? <Link to="/contact">Contact me</Link>
+        </p>
+      </Layout>
+    </div>
+  )
 }
 
-export default IndexPage;
+export default IndexPage
