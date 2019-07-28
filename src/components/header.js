3abb74ddeed393b62d8a238fa840701c989ed5e4
mythLabs <amitbisht744@gmail.com>
2019-07-28 18:35:57 +0530

    Added scoped styling with css modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import {Link} from 'gatsby';
 
+import headerStyles from './header.module.scss';
+
 const Header = () => {
     return (
-        <header>
-            <h1>mythLabs</h1>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link to="/" className={headerStyles.title}>
+                mythLabs
+                </Link>
+                </h1>
             <nav>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/about'>About me</Link></li>
-                    <li><Link to='/contact'>Contact me</Link></li>
-                    <li><Link to='/blog'>Blog</Link></li>
+                <ul className={headerStyles.navList} >
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Home</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>About me</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'>Contact me</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/blog'>Blog</Link></li>
                 </ul>
             </nav>
         </header>
