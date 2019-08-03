a635f29ed08ebfa65c38ca96de2a16fe8b047e14
mythLabs <amitbisht744@gmail.com>
2019-08-03 14:49:42 +0530

    Adding images through markdown and added css to blog and footer pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link, graphql, useStaticQuery } from "gatsby"
 
+import blogStyles from "./blog.module.scss"
+
 import Layout from "../components/layout"
 
 const BlogPage = () => {
@@ -29,10 +31,10 @@ const BlogPage = () => {
         <div>
             <Layout>
             <h1>Blog</h1>
-            <ol>
+            <ol className={blogStyles.posts}>
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return(
-                        <li>
+                        <li className={blogStyles.post}>
                             <Link to={`/blog/${edge.node.fields.slug}`}>
                                 <h2>{edge.node.frontmatter.title}</h2>
                              <p>{edge.node.frontmatter.date}</p>
--- src/posts/gatsby/gatsby.md
+++ src/posts/gatsby/gatsby.md
@@ -6,6 +6,10 @@ date: "28-07-2019"
 So this is ready,
 Cooked freshly
 
+
+![Car](./car.jpg)
+
+
 ##Topics covered
 
 1.Gatsby
